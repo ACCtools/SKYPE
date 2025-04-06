a795b41cfb245d8d147118e691d98f35e3218180
Hyunwoo Ryu <wowo0118@sasa.hs.kr>
2025-04-06 21:25:47 +0900

    Figure change legend

--- 30_depth_analysis.py
+++ 30_depth_analysis.py
@@ -844,7 +844,7 @@ for i, (bnd_loc1, bnd_loc2, cn, is_vir) in enumerate(bnd_cn_data):
 
 circos.colorbar(vmin=0, vmax=2, bounds=(1.01 + off, 0.825, 0.02, 0.1), cmap=cmap,
                 label='Breakend CN', orientation='vertical',
-                colorbar_kws=dict(format=mpl.ticker.FixedFormatter(['0', '2N', 'N']),
+                colorbar_kws=dict(format=mpl.ticker.FixedFormatter(['0', '2N', '4N']),
                                   ticks=mpl.ticker.FixedLocator([0, 1, 2])),
                 label_kws=dict(fontsize=9,),
                 tick_kws=dict(labelsize=8,))
